caab4370ba42ad96356d9a7d56d0e98efa19b243
Hannover22 <gabrielcaramori4@gmail.com>
2025-11-09 22:01:39 -0300

    Desafio concluido

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Data.Common;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -5,6 +7,8 @@ namespace DesafioFundamentos.Models
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
         private List<string> veiculos = new List<string>();
+        private int quantidadeVagas = 255;
+        private int contadorVagas = 0;
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -14,34 +18,44 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            if (contadorVagas < quantidadeVagas)
+            {
+                Console.WriteLine("Digite a placa do veículo para estacionar:");
+                string placa = Console.ReadLine();
+                this.veiculos.Add(placa);
+                this.contadorVagas += 1;
+            }
+            else
+            {
+                Console.WriteLine("O estacionamento esta cheio");
+            }
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+
+            Console.WriteLine("Digite a placa do veículo que deve ser removido:");
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                string numero = Console.ReadLine();
+                int horas = Convert.ToInt32(numero);
+
+                Console.WriteLine("Digite o preço por hora gasta no estacionamento: ");
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                
+                this.veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                this.contadorVagas -= 1;
             }
             else
             {
@@ -55,12 +69,35 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                
+                for (int i = 0; i < veiculos.Count; i++)
+                {
+                    Console.WriteLine($"Vaga: {i} Veiculo: {veiculos[i]}");
+                }
             }
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+
+        public void ConsultarCapacidade()
+        {
+            if (veiculos.Any())
+            {
+                if (this.contadorVagas < this.quantidadeVagas)
+                {
+                    Console.WriteLine($" Total de vagas: {this.quantidadeVagas} Vagas ocupadas: {this.contadorVagas}");
+                    Console.WriteLine($"Vagas disponiveis: {this.quantidadeVagas - this.contadorVagas}");
+                }
+                else
+                {
+                    Console.WriteLine("O estacionamento está cheio");
+                }
+            }
+            else
+            {
+                Console.WriteLine("O estacionamento esta vazio");
             }
         }
     }
